6e4ff251109b3c343033d28017b6a9aca57d5517
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-08-07 21:22:47 -0300

    adiciona descrição as funções

--- inc/sx1276.h
+++ inc/sx1276.h
@@ -1,5 +1,6 @@
 // Registros do SX1276
 #define REG_FIFO            0x00
+#define REG_VERSION         0x42
 #define REG_OP_MODE         0x01
 #define REG_FRF_MSB         0x06
 #define REG_FRF_MID         0x07
@@ -8,13 +9,14 @@
 #define REG_FIFO_ADDR_PTR   0x0D
 #define REG_PAYLOAD_LENGTH  0x22
 #define REG_IRQ_FLAGS       0x12
-#define REG_FIFO_TX_BASE    0x0E
-#define REG_FIFO_RX_BASE    0x0F
+#define REG_FIFO_TX_BASE_ADDR 0x0E
+#define REG_FIFO_RX_BASE_ADDR 0x0F
 #define REG_MODEM_CONFIG1   0x1D
 #define REG_MODEM_CONFIG2   0x1E
 #define REG_MODEM_CONFIG3   0x26
 #define REG_20_PREAMBLE_MSB 0x20
 #define REG_21_PREAMBLE_LSB 0x21
+#define REG_RX_NB_BYTES     0x13
 
 // Define o modo LORA para o chip (o outro é  o FSK/OOK)
 #define MODE_LORA           0x80
--- main.c
+++ main.c
@@ -37,8 +37,8 @@ void unselect_slave(uint chip_select) {
 // Escreve no registrador com endereço ADDRESS o valor VALUE
 void write_register(spi_inst_t *spi_channel, uint8_t address, uint8_t value) {
     uint8_t buffer[2];
-    uint8_t buffer[0] = address | 0x80;
-    uint8_t buffer[1] = value;
+    buffer[0] = address | 0x80;
+    buffer[1] = value;
 
     select_slave(CS_PIN);
     spi_write_blocking(spi_channel, buffer, 2);
@@ -48,8 +48,8 @@ void write_register(spi_inst_t *spi_channel, uint8_t address, uint8_t value) {
 // Realiza a leitura do registrador com endereço ADDRESS
 uint8_t read_register(spi_inst_t *spi_channel, uint8_t address) {
     uint8_t buffer[2];
-    uint8_t buffer[0] = address & 0x7F;
-    uint8_t buffer[1] = 0x00;
+    buffer[0] = address & 0x7F;
+    buffer[1] = 0x00;
 
     // Armazena o valor lido
     uint8_t result[2];
@@ -86,85 +86,125 @@ void sx1276_init() {
     // Reseta o dispositivo
     sx1276_reset();
 
-    // Coloca o chip no modo SLEEP
-    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_SLEEP | MODE_LORA);
+    // Verifica a versão do chip
+    uint8_t version = read_register(SPI_CHANNEL, REG_VERSION);
+    if (version != 0x12) {
+        printf("O chip SX1276 nao foi encontrado! Versao: 0x%02X\n", version);
+    } else {
+        printf("SX1276 OK. Versao: 0x%02X\n", version);
+    }
+
+    // Coloca o chip no modo SLEEP e seleciona o modo LoRa (bit 7 = 1)
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | MODE_SLEEP);
     sleep_ms(10);
 
     // Define a frequência de 915 MHz
     sx1276_set_frequency(915000000);
 
-    // ----
-    write_register(SPI_CHANNEL, REG_FIFO_TX_BASE, 0);
-    sleep_ms(10);
-
     // Configurações do modem
-    write_register(SPI_CHANNEL, REG_MODEM_CONFIG1, 0x72); // BW=125kHz, CR=4/5
-    write_register(SPI_CHANNEL, REG_MODEM_CONFIG2, 0x74); // SF=7, CRC on
-    write_register(SPI_CHANNEL, REG_MODEM_CONFIG3, 0x04); // AgcAutoOn
+    // BW=125 kHz, CR=4/5, Header explícito (ImplicitHeaderMode = 0 -> não é necessário indicar o tamanho do payload no registrador RegPayloadLength)
+    // SF=7 (0111), TxContinuousMode=0 (envio de um pacote apenas), RxPayloadCrcOn=1, SymbTimeout(9:8)=0x00
+    // 0000 (não representa nada), LowDataRateOptimize=0 (desabilitado), AgcAutoOn=1, 00 (reservado)
+    write_register(SPI_CHANNEL, REG_MODEM_CONFIG1, 0x72); // 0b01110010
+    write_register(SPI_CHANNEL, REG_MODEM_CONFIG2, 0x74); // 0b01110100
+    write_register(SPI_CHANNEL, REG_MODEM_CONFIG3, 0x04); // 0b00000100
+
+    // Configuração de preâmbulo igual à 8
     write_register(SPI_CHANNEL, REG_20_PREAMBLE_MSB, 0);
     write_register(SPI_CHANNEL, REG_21_PREAMBLE_LSB, 8);
-    write_register(SPI_CHANNEL, REG_PA_CONFIG, 0x8F); // PA_BOOST +17 dBm
+
+    // Define a potência (17 dBm)
+    write_register(SPI_CHANNEL, REG_PA_CONFIG, 0x8F);
     sleep_ms(10);
 
-    // define modo idle
-    write_register(SPI_CHANNEL, REG_FIFO_ADDR_PTR, 0x00);
+    // Define o endereço do FIFO aonde os dados devem ser colocados
+    // Para descrição detalhada do funcionamento, ler o item 4.1.2.3 - Principle of Operation
+    // Os registradores RegFifoTxBaseAddr e RegFifoRxBaseAddr são definidos para o ponto 0x00 da memória
+    // de modo que todo o espaço disponível do buffer seja utilizado (256 bytes). Por padrão, eles são
+    // definidos para metade da memória (RegFifoRxBaseAddr=0x00 e RegFifoTxBaseAddr=0x80).
+    write_register(SPI_CHANNEL, REG_FIFO_TX_BASE_ADDR, 0x00); // Indica o ponto na memória em que os dados que serão transmitidos estão armazenados
+    write_register(SPI_CHANNEL, REG_FIFO_RX_BASE_ADDR, 0x00); // Indica a posição na memória em que os dados recebidos estarão armazenados
+    // O registrador RegFifoAddrPtr deve ser inicializado com o endereço do buffer em que os dados serão escritos (w) ou lidos (r).
+    // Caso seja realizada a leitura, definir o valor do RegFifoRxBaseAddr. Caso seja realizada a escrita, definir
+    // o valor do registrador RegFifoTxBaseAddr.
+    // Neste caso, ambos os registradores foram definidos para utilizarem o espaço total disponibilizado. Sendo assim,
+    // o registrador RegFifoAddrPtr deve ser inicializado com o valor 0x00 (local a partir do qual os dados recebidos ou transmitidos)
+    // estão armazenados.
+    write_register(SPI_CHANNEL, REG_FIFO_ADDR_PTR, 0x00); // Indica em que endereço do FIFO deve começar a escrever os dados recebidos (TX ou RX) -> indica para qual endereço o ponteiro deve apontar
+    sleep_ms(10);
 
-    // define modo idle
+    // define modo standby
     write_register(SPI_CHANNEL, REG_OP_MODE, MODE_STDBY);
     sleep_ms(10);
 }
 
-void sx1276_transmit(const uint8_t *data, uint8_t len) {
-    write_register(REG_FIFO_ADDR_PTR, 0x00);
+// Realiza a transmissão de dados
+void sx1276_transmit(uint8_t *data, uint8_t len) {
+    // Coloca o chip em modo de standby (necessário para realizar transmissão)
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | MODE_STDBY);
+    sleep_ms(10);
 
+    // Define a posição inicial do ponteiro do buffer do FIFO para o endereço inicial do TX (0x00)
+    write_register(SPI_CHANNEL, REG_FIFO_ADDR_PTR, 0x00);
+
+    // Escreve os dados no buffer do FIFO
     for (int i = 0; i < len; i++) {
-        write_register(REG_FIFO, data[i]);
+        write_register(SPI_CHANNEL, REG_FIFO, data[i]);
     }
 
-    write_register(REG_PAYLOAD_LENGTH, len);
+    // Define o tamanho do payload que será enviado
+    write_register(SPI_CHANNEL, REG_PAYLOAD_LENGTH, len);
 
-    // Modo transmissão
-    write_register(REG_OP_MODE, MODE_LORA | MODE_TX);
+    // Limpa as flags de interrupção
+    write_register(SPI_CHANNEL, REG_IRQ_FLAGS, 0xFF);
 
-    // Espera até TX_DONE (IRQ)
-    while ((read_register(REG_IRQ_FLAGS) & 0x08) == 0) {
+    // Define o modo de transmissão (TX)
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | MODE_TX);
+
+    // Espera até o bit TX_DONE do registrador RegIrqFlags ser setado
+    while ((read_register(SPI_CHANNEL, REG_IRQ_FLAGS) & 0x08) == 0) {
         tight_loop_contents();
     }
 
-    // Limpa flags de IRQ
-    write_register(REG_IRQ_FLAGS, 0xFF);
+    // Limpa as flags de interrupção
+    write_register(SPI_CHANNEL, REG_IRQ_FLAGS, 0xFF);
+
+    // Define o modo de standby
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | MODE_STDBY);
+
+    printf("Transmissão de dados realizada.\n");
 }
 
 int sx1276_receive(uint8_t *buffer, uint8_t max_len) {
     // Modo LoRa + Standby
-    write_register(REG_OP_MODE, MODE_LORA | MODE_STDBY);
-    write_register(REG_IRQ_FLAGS, 0xFF);  // Limpa qualquer flag anterior
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | MODE_STDBY);
+    write_register(SPI_CHANNEL, REG_IRQ_FLAGS, 0xFF);  // Limpa qualquer flag anterior
 
     // Ponteiro FIFO de recepção
-    write_register(REG_FIFO_ADDR_PTR, 0x00);
+    write_register(SPI_CHANNEL, REG_FIFO_ADDR_PTR, 0x00);
 
     // Ativa modo RX contínuo
-    write_register(REG_OP_MODE, MODE_LORA | 0x05);  // RX_CONTINUOUS
+    write_register(SPI_CHANNEL, REG_OP_MODE, MODE_LORA | 0x05);  // RX_CONTINUOUS
 
     // Espera o pacote ser recebido (RX_DONE)
-    while ((read_register(REG_IRQ_FLAGS) & 0x40) == 0) {
+    while ((read_register(SPI_CHANNEL, REG_IRQ_FLAGS) & 0x40) == 0) {
         tight_loop_contents();  // Espera ativamente
     }
 
     // Le o comprimento do payload
-    uint8_t len = read_register(REG_PAYLOAD_LENGTH);
+    uint8_t len = read_register(SPI_CHANNEL, REG_PAYLOAD_LENGTH);
     if (len > max_len) len = max_len;
 
     // Ponteiro para onde o pacote foi armazenado no FIFO
-    uint8_t fifo_rx_current_addr = read_register(0x10); // REG_FIFO_RX_CURRENT_ADDR
-    write_register(REG_FIFO_ADDR_PTR, fifo_rx_current_addr);
+    uint8_t fifo_rx_current_addr = read_register(SPI_CHANNEL, 0x10); // REG_FIFO_RX_CURRENT_ADDR
+    write_register(SPI_CHANNEL, REG_FIFO_ADDR_PTR, fifo_rx_current_addr);
 
     for (int i = 0; i < len; i++) {
-        buffer[i] = read_register(REG_FIFO);
+        buffer[i] = read_register(SPI_CHANNEL, REG_FIFO);
     }
 
     // Limpa flags de IRQ
-    write_register(REG_IRQ_FLAGS, 0xFF);
+    write_register(SPI_CHANNEL, REG_IRQ_FLAGS, 0xFF);
 
     return len;
 }
@@ -232,11 +272,9 @@ int main() {
     sleep_ms(4000);
 
     while (1) {
-        const char *msg = "Ola LoRa";
-        printf("Transmitindo: %s\n", msg);
-        sx1276_transmit((const uint8_t *)msg, strlen(msg));
+        uint8_t msg[] = "Hello, World!";
 
-        printf("Transmissão finalizada.\n");
+        sx1276_transmit(&msg, sizeof(msg) - 1);
 
         sleep_ms(3000);
 
